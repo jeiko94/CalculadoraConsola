2e0af47f871d9fc8916a74a809b5544bfb633f5a
Yeisson Villamil <yeisson.villamil@globalmvm.com>
2024-10-13 17:17:19 -0500

    Agregar archivos de proyecto.

---
+++ CalculadoraConsola/Logica.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CalculadoraConsola;
+
+namespace CalculadoraConsola
+{
+    public class Logica
+    {
+        Operaciones oper = new Operaciones();
+        
+        static void RealizarOperacion(string peracion)
+        {
+            Console.WriteLine("Ingresa un primer numero: ");
+            double num1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingresa un segundo numero: ");
+            double num2 = double.Parse(Console.ReadLine());
+
+            double resultado = 0;
+
+            switch (peracion)
+            {
+                case "sumar":
+                    resultado = Suma(num2 + num1);
+                    break;
+            }
+        }
+    }
+}
---
+++ CalculadoraConsola/Program.cs
@@ -0,0 +1,33 @@
+﻿static void Main(string[] args)
+{
+    bool continuar = true;
+
+    while (continuar)
+    {
+        Console.WriteLine("calculadora de consola.");
+        Console.WriteLine("Seleccione una operación: ");
+        Console.WriteLine("1. Suma");
+        Console.WriteLine("2. Resta");
+        Console.WriteLine("3. Multiplicación");
+        Console.WriteLine("4. División");
+        Console.WriteLine("5. Salir");
+
+        int opcion = int.Parse(Console.ReadLine());
+
+        switch(opcion)
+        {
+            case 1:
+                RealizarOperacion("sumar");
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+        }
+    }
+}
+
